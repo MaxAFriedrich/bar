4bd3499856d80117b8c1bc63e4aa2c1ca3a507f3
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2021-04-24 12:49:42 +0100

    add qrcodes in and fixed some bugs and usability problems

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 function make() {
-    JsBarcode("#barcode", document.getElementById("inp").value, {
-        format: document.getElementById("type").value,
-        lineColor: document.getElementById("color").value,
-        height: document.getElementById("height").value,
-        width: document.getElementById("width").value,
+    if (document.getElementById("type").value == "qr") {
+        document.getElementById("width").style.display = "none";
+        document.getElementById("wrap").innerHTML = new QRCode({
+            msg: document.getElementById("inp").value
+            , dim: document.getElementById("height").value
+            , pad: 4
+            , mtx: -1
+            , ecl: "H"
+            , ecb: 1
+            , pal: [document.getElementById("color").value, "#ffffff"]
+            , vrb: 0
 
-    });
+        }).outerHTML;
+    } else {
+        document.getElementById("width").style.display = "inline";
+        document.getElementById("wrap").innerHTML = `<svg id="barcode"></svg>`;
+        JsBarcode("#barcode", document.getElementById("inp").value, {
+            format: document.getElementById("type").value,
+            lineColor: document.getElementById("color").value,
+            height: document.getElementById("height").value,
+            width: document.getElementById("width").value,
+
+        });
+    }
 }
+
 function print() {
     var html_string = "<script>window.print();</script>" + document.getElementById("wrap").innerHTML;
     document.getElementById('printer').src = "data:text/html;charset=utf-8," + escape(html_string);
@@ -25,11 +43,11 @@ function down() {
     var element = document.createElement('a');
     element.setAttribute('href', img);
     element.setAttribute('download', "bar.png");
-  
+
     element.style.display = 'none';
     document.body.appendChild(element);
-  
+
     element.click();
-  
+
     document.body.removeChild(element);
 }
