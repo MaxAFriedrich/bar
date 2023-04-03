b5bef756208798eb16fd2fa0183d4c03a09c80c8
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2023-04-03 22:13:08 +0100

    Updated theme and removed bloat

--- main.js
+++ main.js
@@ -1,47 +1,16 @@
-function make() {
-    try {
-        height = Math.min(document.getElementById("wrap").clientHeight, document.getElementById("wrap").clientWidth);
-        document.getElementById("wrap").innerHTML = new QRCode({
-            msg: document.getElementById("inp").value
-            , dim: height
-            , pad: 4
-            , mtx: -1
-            , ecl: "H"
-            , ecb: 1
-            , pal: [document.getElementById("color").value, "#ffffff"]
-            , vrb: 0
-
-        }).outerHTML;
-
-    }
-    catch {
-
-    }
-}
-
-function print() {
-    var html_string = "<script>window.print();</script>" + document.getElementById("wrap").innerHTML;
-    document.getElementById('printer').src = "data:text/html;charset=utf-8," + escape(html_string);
-}
-
-function down() {
-
-    const canvas = document.querySelector('canvas');
-    const ctx = canvas.getContext('2d');
-    v = canvg.Canvg.fromString(ctx, document.getElementById("wrap").innerHTML);
-    v.start();
-    //var canvas = document.getElementById("mycanvas");
-    var img = canvas.toDataURL("image/png");
-    //document.getElementById("downer").innerHTML = '<img src="'+img+'"/>';
-
-    var element = document.createElement('a');
-    element.setAttribute('href', img);
-    element.setAttribute('download', "bar.png");
-
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
+function make(element) {
+  const qr_code = document.getElementById("qr_code");
+  try {
+    height = Math.min(qr_code.clientHeight, qr_code.clientWidth);
+    qr_code.innerHTML = new QRCode({
+      msg: element.value,
+      dim: height,
+      pad: 4,
+      mtx: -1,
+      ecl: "H",
+      ecb: 1,
+      pal: ["#000", "#ffffff"],
+      vrb: 0,
+    }).outerHTML;
+  } catch {}
 }
