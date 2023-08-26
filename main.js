9566d49c918b10056cc9b4a816f4267c926d1df8
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2023-08-26 19:09:56 +0100

    Added query string support

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-function make(element) {
-  const qr_code = document.getElementById("qr_code");
-  try {
-    height = Math.min(qr_code.clientHeight, qr_code.clientWidth);
-    qr_code.innerHTML = new QRCode({
-      msg: element.value,
-      dim: height,
-      pad: 4,
-      mtx: -1,
-      ecl: "H",
-      ecb: 1,
-      pal: ["#000", "#ffffff"],
-      vrb: 0,
-    }).outerHTML;
-  } catch {}
+window.onload = () => {
+  function make(text) {
+    const qr_code = document.getElementById("qr_code");
+    try {
+      height = Math.min(qr_code.clientHeight, qr_code.clientWidth);
+      qr_code.innerHTML = new QRCode({
+        msg: text,
+        dim: height,
+        pad: 4,
+        mtx: -1,
+        ecl: "H",
+        ecb: 1,
+        pal: ["#000", "#ffffff"],
+        vrb: 0,
+      }).outerHTML;
+    } catch { }
+  }
+
+  const input = document.getElementById("input")
+  input.value = new URLSearchParams(window.location.search).get("text")
+  input.addEventListener("keyup",()=>{make(input.value)})
+  make("123")
 }
